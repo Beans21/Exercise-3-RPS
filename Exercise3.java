ae5052f4da88db5b430e5e05fe589ef564b67c1c
lschaberg19 <lschaberg19@windwardschool.org>
2016-11-08 08:56:18 -0800

    Okay

---
+++ Exercise3.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Exercise3 {
+
+	public static void main (String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.print("User Stats - ");
+		System.out.print("1. User1 , how many times did you play Max's RPS?" );
+		int user1Games = console.nextInt();
+		System.out.print("2. User1 , how many times did you win in Max's RPS? ");
+		
+		
+	}
+}
